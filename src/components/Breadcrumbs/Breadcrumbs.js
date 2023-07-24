e3d4bd39aff5d0ac864c848778899614c215df37
“go2023” <tetyanago2018@gmail.com>
2023-07-23 21:57:58 -0700

    Breadcrumbs.js was cleaned up

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Link, useLocation} from "react-router-dom";
 
-function Breadcrumbs(props) {
+function Breadcrumbs() {
     const location = useLocation();
-    const pathnames = location.pathname.split('/').filter((u) => u);
+    const pathnames = location.pathname.split('/').filter(u => u);
 
     return (
         <div className="container">
@@ -13,19 +13,22 @@ function Breadcrumbs(props) {
                 const isLast = index === pathnames.length -1;
                 const to = `/${pathnames.slice(0, index + 1).join('/')}`;
 
-                console.log('Where:', to);
+                let breadcrumb = value;
+
+                if(isLast && value.includes("-")) {
+                    breadcrumb = value.split("-").slice(1).join(" ");
+                }
 
                 return isLast ? (
-                    // <span key={to}> | {value}</span>
-                    <span key={to}> | {value}</span>
-                ) : (
-                    <>
-                        <span> | </span>
-                        <Link key={to} to={to}>
-                            {value}
-                        </Link>
-                    </>
-                );
+                                    <span key={to}> | {breadcrumb} </span>
+                                ) : (
+                                        <>
+                                            <span> | </span>
+                                            <Link key={to} to={to}>
+                                                {breadcrumb}
+                                            </Link>
+                                        </>
+                                    );
             })}
         </div>
     );
